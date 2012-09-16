8fb0fe34e6b034fb06a0fa325c30af29fd9a5b72
Marcin Kulik <marcin.kulik@gmail.com>
2012-09-16 15:16:53 +0100

    Generate gravatar based avatar URL for BrowserID accounts

--- lib/omni_auth_helper.rb
+++ lib/omni_auth_helper.rb
@@ -5,6 +5,10 @@ class OmniAuthHelper
       auth["info"]["image"]
     elsif auth["provider"] == "github"
       auth["extra"]["raw_info"]["avatar_url"]
+    elsif auth["provider"] == "browser_id"
+      email = auth["uid"]
+      hash = Digest::MD5.hexdigest(email.to_s.downcase)
+      "http://gravatar.com/avatar/#{hash}?s=64"
     end
   end
 
